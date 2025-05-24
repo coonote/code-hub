7125c8826e8b42002b43c9baea86fc1a85e8bb8f
moon <moon@moondeMBP.lan>
2025-05-24 23:13:48 +0800

    deploy

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,7 +0,0 @@
-import { GitContributors } from "/Users/moon/code-hub/vuepress-docs/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-  },
-};
--- docs/.vuepress/.temp/markdown-tab/config.js
+++
@@ -1,8 +0,0 @@
-import { CodeTabs } from "/Users/moon/code-hub/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
-import "/Users/moon/code-hub/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("CodeTabs", CodeTabs);
-  },
-};
